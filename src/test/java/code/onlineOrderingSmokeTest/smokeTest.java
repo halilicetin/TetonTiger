180f582fbac075f977ccf0bfe61c848397f42198
halilicetin <cetinhalilstudent@gmail.com>
2022-06-20 16:06:26 -0600

    some files are created

---
+++ src/test/java/code/onlineOrderingSmokeTest/smokeTest.java
@@ -0,0 +1,11 @@
+package code.onlineOrderingSmokeTest;
+
+import org.testng.annotations.Test;
+
+public class smokeTest {
+        @Test
+                public void firsttest(){
+            System.out.println("lololololololo");
+
+    }
+}
